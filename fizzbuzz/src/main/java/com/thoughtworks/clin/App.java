9a4699e78a6c3191e84e0ebd4638966cd615e65e
Lin Chen <lc1990linux@gmail.com>
2020-01-10 11:51:58 +0800

    [round1] 15mins works

--- fizzbuzz/src/main/java/com/thoughtworks/clin/App.java
+++ fizzbuzz/src/main/java/com/thoughtworks/clin/App.java
@@ -5,7 +5,34 @@ package com.thoughtworks.clin;
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		for( int i = 0; i < 120; i++) {
+			String result = fizzBuzz(i+1);
+			System.out.println(result);
+		}
+	}
+
+	public static String fizzBuzz(Integer number) {
+		if(number % 3 == 0) {
+			if(number % 5 == 0) {
+				if(number % 7 == 0) {
+					return "FizzBuzzWhizz";
+				}
+				return "FizzBuzz";
+			} else if(number % 7 == 0) {
+				return "FizzWhizz";
+			}
+			return "Fizz";
+		}
+		if(number % 5 == 0) {
+			return "Buzz";
+		}
+		if(number % 7 == 0) {
+			return "Whizz";
+		}
+		return number.toString();
+	}
+
+
 }
--- fizzbuzz/src/test/java/com/thoughtworks/clin/AppTest.java
+++ fizzbuzz/src/test/java/com/thoughtworks/clin/AppTest.java
@@ -1,33 +1,51 @@
 package com.thoughtworks.clin;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-	/**
-	 * Create the test case
-	 *
-	 * @param testName name of the test case
-	 */
-	public AppTest(String testName) {
-		super(testName);
+import org.junit.Test;
+
+public class AppTest{
+
+	@Test
+	public void shouldSayHisNumberNormally() {
+		assertEquals(App.fizzBuzz(1), "1");
+		assertEquals(App.fizzBuzz(2), "2");
 	}
 
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(AppTest.class);
+	@Test
+	public void shouldSayFizzIfNumberTimes3() {
+		assertEquals(App.fizzBuzz(3), "Fizz");
+		assertEquals(App.fizzBuzz(6), "Fizz");
+		assertEquals(App.fizzBuzz(9), "Fizz");
 	}
 
-	/**
-	 * Rigourous Test :-)
-	 */
-	public void testApp() {
-		assertTrue(true);
+	@Test
+	public void shouldSayBuzzIfNumberTimes5() {
+		assertEquals(App.fizzBuzz(5), "Buzz");
+		assertEquals(App.fizzBuzz(10), "Buzz");
 	}
+
+	@Test
+	public void shouldSayWhizzIfNumberTimes7() {
+		assertEquals(App.fizzBuzz(7), "Whizz");
+		assertEquals(App.fizzBuzz(14), "Whizz");
+	}
+
+	@Test
+	public void shouldSayFizzBuzzIfNumberTimes3and5() {
+		assertEquals(App.fizzBuzz(15), "FizzBuzz");
+		assertEquals(App.fizzBuzz(30), "FizzBuzz");
+	}
+
+	@Test
+	public void shouldSayFizzWhizzIfNumberTimes3and7() {
+		assertEquals(App.fizzBuzz(21), "FizzWhizz");
+		assertEquals(App.fizzBuzz(42), "FizzWhizz");
+	}
+
+	@Test
+	public void shouldSayFizzBuzzWhizzIfNumberTimes3and5and7() {
+		assertEquals(App.fizzBuzz(105), "FizzBuzzWhizz");
+	}
+
 }
