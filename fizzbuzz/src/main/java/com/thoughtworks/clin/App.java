c7ea63bb828b1dafc743e977c4bcc328a87efc6e
Lin Chen <lc1990linux@gmail.com>
2020-01-10 15:42:05 +0800

    [round1][Chen] finish all scenario

--- fizzbuzz/src/main/java/com/thoughtworks/clin/App.java
+++ fizzbuzz/src/main/java/com/thoughtworks/clin/App.java
@@ -2,7 +2,6 @@ package com.thoughtworks.clin;
 
 /**
  * Hello world!
- *
  */
 public class App {
 	public static void main(String[] args) {
@@ -14,6 +13,30 @@ public class App {
 	}
 
 	public static String fizzBuzz(Integer number) {
+		if(Integer.toString(number).contains("7")) {
+			if (number % 7 == 0) {
+				return "Whizz";
+			}
+			return number.toString();
+		}
+		// rule 6 will ignore 4 & 5 and ignore times 3
+		if(Integer.toString(number).contains("5")) {
+			if (number % 5 == 0) {
+				if (number % 7 == 0) {
+					return "BuzzWhizz";
+				}
+				return "Buzz";
+			}
+			if (number % 7 == 0) {
+				return "Whizz";
+			}
+			return number.toString();
+		}
+		// rule 4 & 5 will ignore rule 2 & 3
+		if(Integer.toString(number).contains("3")) {
+			return "Fizz";
+		}
+		// rule 2&3
 		if(number % 3 == 0) {
 			if(number % 5 == 0) {
 				if(number % 7 == 0) {
@@ -26,13 +49,16 @@ public class App {
 			return "Fizz";
 		}
 		if(number % 5 == 0) {
+			if(number % 7 == 0) {
+				return "BuzzWhizz";
+			}
 			return "Buzz";
 		}
 		if(number % 7 == 0) {
 			return "Whizz";
 		}
+		// rule 1
 		return number.toString();
 	}
-
 
 }
--- fizzbuzz/src/test/java/com/thoughtworks/clin/AppTest.java
+++ fizzbuzz/src/test/java/com/thoughtworks/clin/AppTest.java
@@ -33,8 +33,8 @@ public class AppTest{
 
 	@Test
 	public void shouldSayFizzBuzzIfNumberTimes3and5() {
-		assertEquals(App.fizzBuzz(15), "FizzBuzz");
-		assertEquals(App.fizzBuzz(30), "FizzBuzz");
+		assertEquals(App.fizzBuzz(60), "FizzBuzz");
+		// assertEquals(App.fizzBuzz(30), "FizzBuzz");
 	}
 
 	@Test
@@ -45,7 +45,70 @@ public class AppTest{
 
 	@Test
 	public void shouldSayFizzBuzzWhizzIfNumberTimes3and5and7() {
-		assertEquals(App.fizzBuzz(105), "FizzBuzzWhizz");
+		assertEquals(App.fizzBuzz(210), "FizzBuzzWhizz");
 	}
 
+	@Test
+	public void shouldSayBuzzWhizzIfNumberTimes5and7() {
+		// assertEquals(App.fizzBuzz(35), "BuzzWhizz");
+		assertEquals(App.fizzBuzz(140), "BuzzWhizz");
+	}
+
+	@Test
+	public void shouldSayFizzIfNumberContains3() {
+		assertEquals(App.fizzBuzz(13), "Fizz");
+		assertEquals(App.fizzBuzz(23), "Fizz");
+		assertEquals(App.fizzBuzz(33), "Fizz");
+		assertEquals(App.fizzBuzz(30), "Fizz");
+		assertEquals(App.fizzBuzz(31), "Fizz");
+		assertEquals(App.fizzBuzz(32), "Fizz");
+		assertEquals(App.fizzBuzz(33), "Fizz");
+		assertEquals(App.fizzBuzz(34), "Fizz");
+		assertEquals(App.fizzBuzz(36), "Fizz");
+		assertEquals(App.fizzBuzz(38), "Fizz");
+		assertEquals(App.fizzBuzz(39), "Fizz");
+		assertEquals(App.fizzBuzz(43), "Fizz");
+	}
+
+	@Test
+	public void shouldSayBuzzIfNumberContains5andTimes5() {
+		assertEquals(App.fizzBuzz(50), "Buzz");
+	}
+
+	@Test
+	public void shouldSayWhizzIfNumberContains5andTimes7() {
+		assertEquals(App.fizzBuzz(56), "Whizz");
+	}
+
+	@Test
+	public void shouldSayBuzzWhizzIfNumberContains5andtimes5and7() {
+		assertEquals(App.fizzBuzz(35), "BuzzWhizz");
+		assertEquals(App.fizzBuzz(105), "BuzzWhizz");
+	}
+
+	@Test
+	public void shouldSayNumberIfNumberContains5andnotTimes5or7EvenTime3() {
+		assertEquals(App.fizzBuzz(51), "51");
+		assertEquals(App.fizzBuzz(52), "52");
+		assertEquals(App.fizzBuzz(53), "53");
+		assertEquals(App.fizzBuzz(54), "54");
+	}
+
+	@Test
+	public void shouldSayWhizzIfNumberContains7andTimes7() {
+		assertEquals(App.fizzBuzz(7), "Whizz");
+		assertEquals(App.fizzBuzz(70), "Whizz");
+		assertEquals(App.fizzBuzz(77), "Whizz");
+	}
+
+	@Test
+	public void shouldSayNumberIfNumberContains7andnotTime7() {
+		assertEquals(App.fizzBuzz(17), "17");
+		assertEquals(App.fizzBuzz(27), "27");
+		assertEquals(App.fizzBuzz(37), "37");
+		assertEquals(App.fizzBuzz(47), "47");
+		assertEquals(App.fizzBuzz(57), "57");
+		assertEquals(App.fizzBuzz(67), "67");
+		assertEquals(App.fizzBuzz(71), "71");
+	}
 }
